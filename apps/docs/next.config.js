0bb77f2218056ea324daa36f079b1cfdcc5c459b
이성준 leeseongjun <jvden@commerceon.co.kr>
2025-04-03 11:32:03 +0900

    docs domain setting 9

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	trailingSlash: false,
-	// basePath: '/docs',
-	// assetPrefix: '/docs'
+	basePath: '/docs',
+	assetPrefix: '/docs'
 };
 
 export default nextConfig;
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
 		return [
 			{
 				source: "/docs/:path*",
-				destination: "https://turborepo-next-docs.vercel.app/:path*",
+				destination: "https://turborepo-next-docs.vercel.app/docs/:path*",
 			},
 			{
 				source: "/react-app/:path*",
-				destination: "https://turborepo-react-app.vercel.app/:path*",
+				destination: "https://turborepo-react-app.vercel.app/react-app/:path*",
 			},
 			{
 				source: "/react-vite/:path*",
