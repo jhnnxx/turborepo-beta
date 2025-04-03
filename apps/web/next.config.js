c0eb8a1a0d2d14b4cec91d2e348a5a2fb8b12da0
이성준 leeseongjun <jvden@commerceon.co.kr>
2025-04-03 10:37:10 +0900

    first commit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: "/docs/:path*",
+				destination: "http://localhost:3001/docs/:path*",
+			},
+			{
+				source: "/react-vite/:path*",
+				destination: "http://localhost:5173/react-vite/:path*",
+			},
+			{
+				source: "/react-app/:path*",
+				destination: "http://localhost:4000/react-app/:path*",
+			}
+		];
+	},
+};
 
 export default nextConfig;
